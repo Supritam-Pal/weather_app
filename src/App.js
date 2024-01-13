59aa15ce489155a742c6af1423c7a7152e2ff762
Supritam-Pal <131386290+Supritam-Pal@users.noreply.github.com>
2024-01-13 22:16:58 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react'
+//import axios from axios;
 
-function App() {
+const App = () => {
+
+  //const url =`https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=f38eb1b50f2e5fa85215cca47aa3edf1`
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=f38eb1b50f2e5fa85215cca47aa3edf1`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Dallas</p>
+          </div>
+          <div className='temp'>
+              <h1>60F</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+         </div>
+                 
+             <div className='bottom'>
+              <div className='feels'>
+                65F
+              </div>
+              <div className='humidity'><p>20%</p></div>
+              <div className='wind'>
+                  12MPH
+              </div>
+             </div>
+      </div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    key: "f38eb1b50f2e5fa85215cca47aa3edf1",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
